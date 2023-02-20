6518edbd15a00f710ed4129fc907b0a05c6c770f
Deepak Mandal <dm6593120@gmail.com>
2023-02-20 21:03:32 +0530

    This is not final version

--- src/App.js
+++ src/App.js
@@ -1,24 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Counted from "./components/Counted";
+import React, { useState } from "react";
+
+let word = 0,
+  character = 0;
+let pagemode = "bi-lightbulb-fill";
+let navbg = "bg-primary";
+let btncolor = "btn-primary";
 
 function App() {
+  const count = (event) => {
+    setText(event.target.value);
+    word = text.split(" ").length;
+    character = text.length;
+  };
+  const capitalize = () => {
+    let newText = text.toUpperCase();
+    console.log(newText);
+    setText(newText);
+  };
+  const [text, setText] = useState("");
+  let [mode, setMode] = useState("light");
+  const changeMode = () => {
+    if (mode === "light") {
+      mode = "dark";
+      setMode(
+        (navbg = "bg-dark"),
+        (pagemode = "bi-lightbulb"),
+        (btncolor = "btn-dark")
+      );
+    } else {
+      mode = "light";
+      setMode(
+        (pagemode = "bi-lightbulb-fill"),
+        (navbg = "bg-primary"),
+        (btncolor = "btn-primary")
+      );
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <nav className={`navbar ${navbg}`}>
+        <div className="container-fluid">
+          <span className="navbar-brand mb-0 h1">Text Counter</span>
+        </div>
+        <div className="cTime">
+          
+        </div>
+
+        <span className="counted">
+          <Counted words={word} characters={character} />
+          <button
+            type="button"
+            className={`btn ${btncolor} bulbtn`}
+            onClick={changeMode}
+          >
+            <i className={`bi ${pagemode} fa-lg`}></i>
+          </button>
+        </span>
+      </nav>
+      <div className="container">
+        <div className="mb-3">
+          <h1 className="heading">Enter Text Here</h1>
+          <textarea
+            className="form-control"
+            id="exampleFormControlTextarea1"
+            rows="3"
+            value={text}
+            onChange={count}
+          ></textarea>
+          <button
+            type="button"
+            className={`btn ${btncolor} my-3 mx-3`}
+            onClick={capitalize}
+          >
+            Uppercase
+          </button>
+        </div>
+      </div>
+    </>
   );
 }
 
---
+++ src/components/Counted.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Counted(props) {
+  return (
+    <span>Words: {props.words} Characters: {props.characters}&ensp;</span>
+  )
+}
